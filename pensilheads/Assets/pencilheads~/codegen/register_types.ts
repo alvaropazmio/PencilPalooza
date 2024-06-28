7b28309745cf986db67d9f87e662426a236361bb
llllkatjallll <katja.rempel@yahoo.de>
2024-06-28 21:28:09 +0200

    networking tests

---
+++ pensilheads/Assets/pencilheads~/codegen/register_types.ts
@@ -0,0 +1,10 @@
+﻿/* eslint-disable */
+import { TypeStore } from "@needle-tools/engine"
+
+// Import types
+import { LineInstanceHandler } from "../LinesManager.js";
+import { LinesManager } from "../LinesManager.js";
+
+// Register types
+TypeStore.add("LineInstanceHandler", LineInstanceHandler);
+TypeStore.add("LinesManager", LinesManager);
