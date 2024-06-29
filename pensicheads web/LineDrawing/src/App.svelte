2ef0126acce68ca0ac640c15e37d474c3cd39544
llllkatjallll <katja.rempel@yahoo.de>
2024-06-30 01:09:28 +0200

    added more sounds

--- pensicheads web/LineDrawing/src/App.svelte
+++ pensicheads web/LineDrawing/src/App.svelte
@@ -1,24 +1,46 @@
 <script lang="ts">
     //import from prncilheads.npm
-      import { CurrentState, AppState } from 'pencilheads.npm/StateManager'; 
+    import { CurrentState, AppState } from "pencilheads.npm/StateManager";
 
-      function setState(state: AppState) {
+    function setState(state: AppState) {
         console.log("Set STATE to: " + state);
         $CurrentState = state;
     }
-
-    </script>
-
-
+</script>
 
 {#if $CurrentState === AppState.Start}
-
-<h1>START GAME</h1>
-
-<button on:click={() => setState(AppState.Level1)}> Level 1</button>
+    <div class="background">
+        <button on:click={() => setState(AppState.Level1)}> Start Game</button>
+    </div>
 {/if}
-    
-    
-    <style>
+
+{#if $CurrentState === AppState.Level1}
+    <!-- <button on:click={() => setState(AppState.Level1)}> Level 1</button> -->
+{/if}
+
+{#if $CurrentState === AppState.Level1}{/if}
+
+<style>
+    .background {
+        width: 100vw;
+        background-color: #fbda61;
+        background-image: linear-gradient(45deg, #fbda61 0%, #f46d25 100%);
+
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    button {
+        padding: 10px 20px;
+        font-size: 60px;
+        background-color: #000000;
+        color: white;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
     /* Style goes here*/
-    </style>
+</style>
